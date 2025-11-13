ba4623ec476c73a8e8eebd19d25b09315220bb11
Workshop Participant <participant@example.com>
2025-11-13 22:15:25 +0000

    initial

--- modules/terraform-aws-eks/versions.tf
+++ modules/terraform-aws-eks/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.3.2"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.95"
+      version = ">= 6.15"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.0"
+      version = ">= 4.0"
     }
     time = {
       source  = "hashicorp/time"
